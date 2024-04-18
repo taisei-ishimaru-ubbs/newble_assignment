df07606343639b316f11b5a9083a5edb5c439fd6
taisei.ishimaru <taisei.ishimaru@human.ait.kyushu-u.ac.jp>
2024-04-18 21:23:14 +0900

    update: ヒストグラムをpdfで保存するように変更

--- 4_brightness_histgram_comparison.py
+++ 4_brightness_histgram_comparison.py
@@ -5,17 +5,14 @@ import numpy as np
 def load_image_in_grayscale(path):
     return Image.open(path).convert('L')
 
-# ヒストグラムを表示する関数
 def plot_histogram(image, label, ax, color):
     data = np.array(image)
     ax.hist(data.ravel(), bins=256, color=color, alpha=0.5, label=label, range=[0,256])
     ax.legend(loc='upper right')
 
-# 画像のパス
 image_path1 = './Lenna.bmp'
 image_path2 = './Pepper.bmp'
 
-# 画像を読み込み
 image1 = load_image_in_grayscale(image_path1)
 image2 = load_image_in_grayscale(image_path2)
 
@@ -28,5 +25,7 @@ plot_histogram(image2, 'Image 2', ax, 'blue')
 ax.set_title('Brightness Histogram Comparison')
 ax.set_xlabel('Pixel Intensity')
 ax.set_ylabel('Frequency')
+
+plt.savefig('histogram_comparison.pdf')
 
 plt.show()
