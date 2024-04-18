6c05c016994d6102442364de6840e683e720dbbb
taisei.ishimaru <taisei.ishimaru@human.ait.kyushu-u.ac.jp>
2024-04-18 21:22:14 +0900

    update: 日本語化

--- 1_matrix_operations.py
+++ 1_matrix_operations.py
@@ -6,16 +6,12 @@ b = np.array([[2, 5], [1, 2]])
 add = a + b
 
 sub = a - b
-mult = a * b
 
-# Matrix product
 prod = np.dot(a, b)
 
-# Element-wise division (be cautious with division by zero)
 div = a / b
 
-print("addition result:\n", add)
-print("Subtraction result:\n", sub)
-print("Element-wise multiplication result:\n", mult)
-print("Matrix product result:\n", prod)
-print("Element-wise division result:\n", div)
+print("加算結果:\n", add)
+print("減算結果:\n", sub)
+print("行列積結果:\n", prod)
+print("除算結果:\n", div)
