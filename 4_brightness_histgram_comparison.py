5201a0e09f5ab329beb7bb2c9f29a183cba4eb92
taisei.ishimaru <taisei.ishimaru@human.ait.kyushu-u.ac.jp>
2024-04-18 22:26:58 +0900

    update: 日本語に変更

--- 4_brightness_histgram_comparison.py
+++ 4_brightness_histgram_comparison.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 import numpy as np
 
 def load_image_in_grayscale(path):
@@ -16,15 +17,21 @@ image_path2 = './Pepper.bmp'
 image1 = load_image_in_grayscale(image_path1)
 image2 = load_image_in_grayscale(image_path2)
 
+font_path = '/usr/share/fonts/truetype/migmix/migmix-2m-bold.ttf'
+font_prop = FontProperties(fname=font_path)
+plt.rcParams["font.family"] = font_prop.get_name()
+
+plt.rcParams.update({'font.size': 16})
 
 fig, ax = plt.subplots(figsize=(10, 6))
 
-plot_histogram(image1, 'Image 1', ax, 'orange')
-plot_histogram(image2, 'Image 2', ax, 'blue')
+plot_histogram(image1, 'Lenna', ax, 'orange')
+plot_histogram(image2, 'Pepper', ax, 'blue')
 
-ax.set_title('Brightness Histogram Comparison')
-ax.set_xlabel('Pixel Intensity')
-ax.set_ylabel('Frequency')
+ax.set_title('輝度ヒストグラムの比較')
+ax.set_xlabel('ピクセル強度')
+ax.set_ylabel('頻度')
+
 
 plt.savefig('histogram_comparison.pdf')
 
